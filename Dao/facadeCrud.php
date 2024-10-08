1a7b1ad384d59b846fb9fb6cfc1b0e142bf32dbd
Valter Filho <valter.filho7@aluno.enova.educacao.ba.gov.br>
2024-10-08 09:24:40 -0300

    Criação da classe "FacadeCrud" que é responsável por unificar os métodos de crud do padrão de projeto DAO, facilitando a interação do Cliente com os subsistemas de Crud.

---
+++ Dao/facadeCrud.php
@@ -0,0 +1,50 @@
+<?php 
+
+    class FacadeCrud {
+
+        private $usuarioDao;
+        private $produtoDao;
+
+        public function __construct() {
+
+            $this->usuarioDao = new CrudUsuarioDaoImplementacao();
+            $this->produtoDao = new CrudProdutoDaoImplementacao();
+            
+        }
+
+
+        public function createEntidade($tipoEntidade, $dadosEntidade) {
+
+            switch ($tipoEntidade) {
+
+                case "usuario": 
+
+                    // Instanciar um objeto usuário.
+                    //$usuario = new Usuario();
+                    // Passar os dados da entidade para o objeto antes de usar o método de create.
+                    //$usuario->setEmail($dadosEntidade['email']);
+
+                    // Utilizando o método de criação do objeto usuário do padrão de projeto DAO.
+                    // return $this->usuarioDao->createUsuario($usuario);
+                
+                case "arduino":
+                    
+                    
+                    $fabricaArduino = new ArduinoConcreteCreator();
+
+                    $arduino = $fabricaArduino->criarProduto($dadosEntidade['imagem'], $dadosEntidade['nome'], $dadosEntidade['valor'], $dadosEntidade['quantidade'], $dadosEntidade['categoria'], $dadosEntidade['tipo'], $dadosEntidade['categoria']);
+
+                    return $this->produtoDao->createProduto($arduino);
+                
+                default:
+                    
+                    throw new Exception("Entidade inválida: " . $tipoEntidade);
+                    
+            }
+
+        }
+
+
+    }
+
+?>
